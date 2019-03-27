ae446c7f6c62f331d858c1d92ce8de49dff97428
e-9 <velazquez.abrahamvb@gmail.com>
2019-03-27 06:21:08 -0600

    Tokens lecture complete

--- router.js
+++ router.js
@@ -11,7 +11,9 @@ const handlers = require("./lib/handlers");
 const router = {
   ping: handlers.ping,
   notFound: handlers.notFound,
-  users: handlers.users
+  users: handlers.users,
+  tokens: handlers.tokens,
+  checks: handlers.checks
 };
 
 module.exports = router;
